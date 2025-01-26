a4690a970af862a90c21a43fe214dd4c47aac3a5
aryan88111 <76144922+aryan88111@users.noreply.github.com>
2025-01-27 02:46:57 +0530

    build

--- src/components/Background3D.jsx
+++ src/components/Background3D.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial } from '@react-three/drei';
-import * as random from 'maath/random/dist/maath-random.esm';
+// import * as random from 'maath/random/dist/maath-random.esm';
 
 const ParticleField = () => {
   const ref = useRef();
